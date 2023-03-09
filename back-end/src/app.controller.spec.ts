089d0b0bfb63ecc46b6a72a484116c7c93214789
Yorí Galisteu <yorigcsdev@gmail.com>
2023-03-09 13:45:16 -0300

    feat: replace hello world to status

--- back-end/src/app.controller.spec.ts
+++ back-end/src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return status API', () => {
+      expect(appController.getStatus()).toBe('API working!');
     });
   });
 });
