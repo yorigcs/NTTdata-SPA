97f7e760c629ac1e198d4cbe06475eb4c59d275c
Yorí Galisteu <yorigcsdev@gmail.com>
2023-03-09 21:19:55 -0300

    feat: config env

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MoviesModule } from './movies/movies.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), MoviesModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,9 +1,10 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { env } from './config/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(env.port);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(process.env.PORT);
 }
 bootstrap();
