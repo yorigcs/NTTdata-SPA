2ea4193c7a0775f26c830200c0ac254f4dbb51ef
Yorí Galisteu <yorigcsdev@gmail.com>
2023-03-10 04:34:28 -0300

    refactor: move baseURL to process

--- front-end/src/external/requests.ts
+++ front-end/src/external/requests.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseURL = 'http://localhost:8000/api'
+const baseURL = process.env.REACT_APP_BASE_URL || 'http://localhost:8000/api'
 
 export const fetchMoviesListPreview = async (name: string,page = 1) => {
     return await axios.post(`${baseURL}/movies`, {name,page})
